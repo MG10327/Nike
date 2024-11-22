cce046923651142f43d8fabdab15d3d01f818536
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-21 21:45:16 -0500

    start setup and fix imports

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { offer } from '../assets/images'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className="flex-1">
+        <img src={offer} alt="Shoe Promotion" width={773} height={687} className='object-contain w-full' />
+      </div>
+
+      <div className="flex flex-1 flex-col">
+        <h2 className='text-4xl font-palanquin font-bold'><span className='text-coral-red'>Special</span> Offer</h2>
+      </div>
+    </section>
   )
 }
 
