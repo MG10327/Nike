0c64bb1982614bd47df02f61d1baa128de7afa69
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-21 21:57:08 -0500

    start basic structure and fix imports

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
-  )
-}
+    <section className='max-container'>
+      <h3 className="font-palanquin text-center text-4xl font-bold">What Our <span className="text-coral-red">Customers</span> Say</h3>
+    </section>
+  );
+};
 
-export default CustomerReviews
+export default CustomerReviews;
